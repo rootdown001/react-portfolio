a0d5647279e8926a9507aa5c000c7223c63d57f3
Lance Anderson <lance.a.585@gmail.com>
2024-01-25 20:42:46 -0800

    ADD initial layout - Added initial layout, utility classes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        burtons: "burtons",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
